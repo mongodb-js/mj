b7f3ec528c541e5e2ef61ee9ef9e046cbcbfe998
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-03-12 23:16:31 +1100

    added first check for docs.

--- commands/check.js
+++ commands/check.js
@@ -1,7 +1,32 @@
-module.exports = function (args) {
-  // defaults
-  args['<directory>'] = args['<directory>'] || '.';
+var fs = require('fs'),
+    glob = require('glob').sync;
 
-  // @todo: implementation
-  console.log(args);
+function test_docs(args) {
+  if (args['--skip-docs']) {
+    return 'skipped';
+  }
+  
+  // check if non-empty README.* exists or a non-empty ./docs folder
+  var readme = glob(args['<directory>'] + '/README.*');
+
+  if (readme.length > 0) {
+    // @todo: check if non-empty
+    return true;
+  }
+
+  // no readme. is there a non-empty ./docs directory?
+  var docs = glob(args['<directory>'] + '/docs/*');
+  return (docs.length > 0);
+}
+
+
+module.exports = function (args) {
+  args['<directory>'] = args['<directory>'] || '.';
+  
+  var results = {
+    docs: test_docs(args)
+  };
+
+  // @todo return true/false based on results
+  console.log(results);
 }
