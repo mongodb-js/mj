d5043d17f89e00b71f226bad7955a963cea7f2ef
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-06-21 16:37:54 +1000

    template dependency tree implemented.

--- commands/create.js
+++ commands/create.js
@@ -5,10 +5,18 @@ var format = require('util').format;
 var Khaos = require('khaos');
 var path = require('path');
 var fs = require('fs');
-
 var executor = require('../util/executor');
 
-var TEMPLATES = ['empty', /* 'cli', 'view', 'spa', 'full', 'util', 'doc' */];
+// Template dependency tree
+var templateDeps = {
+  empty: null,
+  cli: 'empty',
+  view: 'empty',
+  util: 'empty',
+  doc: 'empty',
+  spa: 'view',
+  full: 'spa'
+};
 
 module.exports = function(args, done) {
 
@@ -16,15 +24,12 @@ module.exports = function(args, done) {
     /**
      * make sure the template exists.
      */
-
     'template': function (callback) {
-      if (TEMPLATES.indexOf(args['<template>']) === -1) {
-        return callback(new Error(format('Unknown template "%s". Run `mj help create` for list of valid templates.', args['<template>'])));
+      if (Object.keys(templateDeps).indexOf(args['<template>']) === -1) {
+        return callback(new Error(format('Unknown template "%s". Run ' +
+          '`mj help create` for list of valid templates.', args['<template>'])));
       }
-      var khaos = new Khaos(path.join(__dirname, '../templates/', args['<template>']));
-
-      // return khaos instance in results
-      return callback(null, khaos);
+      return callback(null);
     },
 
     /**
@@ -34,23 +39,29 @@ module.exports = function(args, done) {
       fs.readdir(args['<directory>'], function (err, files) {
         // error here means it does not exist, which is good.
         if (err) return callback(null, args['<directory>']);
-        if ((files.length) > 0) return callback(new Error(format('destination directory %s is not empty.', path.resolve(args['<directory>']))));
+        if ((files.length) > 0) return callback(new Error(format('destination ' +
+          'directory %s is not empty.', path.resolve(args['<directory>']))));
         return callback(null, args['<directory>']);
       });
-    },
-
-    /**
-     * now copy template to destination with khaos, prompt for handlebar variables
-     */
-    'copy': [
-      'template',
-      'destination',
-      function(callback, results) {
-        var khaos = results.template;
-        khaos.generate(results.destination, callback);
-      }
-    ]
+    }
   };
+
+  // add khaos.generate tasks for each template in the dependency tree
+  var generateTemplate = function (template) {
+    return function (callback, results) {
+      var khaos = new Khaos(path.join(__dirname, '../templates/', template));
+      khaos.generate(results.destination, callback);
+    };
+  };
+  var curr = args['<template>'];
+  while (curr) {
+    var parent = templateDeps[curr];
+    // non-root depends on parent template, root on 'template' and 'destination'
+    tasks[curr] = parent ?
+      [parent, generateTemplate(curr)] :
+      ['template', 'destination', generateTemplate(curr)];
+    curr = parent;
+  }
 
   var options = {
     name: 'create',  // this name is used when --verbose is not set
