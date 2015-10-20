1e37abc31c9a99bf3647c595faf2a8a546b5e724
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-20 17:59:01 -0400

    :fire: move cli template out temporarily

--- templates/cli/lib/load_docopt.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-sync:0 */
-var path = require('path');
-var fs = require('fs');
-
-module.exports = function(command) {
-  var clifile = path.join(__dirname, '../docopts', command + '.docopt');
-  return fs.readFileSync(clifile, 'utf-8');
-};
